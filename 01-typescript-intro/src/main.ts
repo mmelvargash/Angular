6207632dfdb418c4d76967936cd65b5a9e9ac0d0
mmelvargash <melannyvargashernandez@gmail.com>
2024-12-27 13:01:16 -0600

    add vite app

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!
+.innerHTML = 
+`
+hola mundo
+`
+console.log('Hola mundo');
